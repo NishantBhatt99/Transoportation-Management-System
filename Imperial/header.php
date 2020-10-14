bccaef9c2e5102cee13ea597cdcadb6579b866d9
NishantBhatt99 <54299463+NishantBhatt99@users.noreply.github.com>
2020-10-14 18:52:44 +0530

    Bus Transportation  System
    This is a project which i worked under subject call web technology.

---
+++ Imperial/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+  <head>
+    
+    <header id="header">
+      <div class="container">
+        <div id="logo" class="pull-left">
+          <a href="#hero"><img src="logo1.jpg"></img></a>
+          
+        </div>
+        <nav id="nav-menu-container">
+          <ul class="nav-menu">
+            <li class="menu-active"><a href="#hero">Home</a></li>
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="services.html">services</a></li>
+            <li><a href="gallery.html">gallery</a></li>
+            <li><a href="team.html">Team</a></li>
+            <li class="menu-has-children"><a href="">Bus Details</a>
+              <ul>
+                <li><a href="busInfo.html">Bus Info.</a></li>
+                <li class="menu-has-children"><a href="#">Search Bus</a>
+                  <ul>
+                    <li><a href="#">Pick Up route</a></li>
+                    <li><a href="#">Drop route</a></li>
+                    <li><a href="#">View Map</a></li>
+                    <li><a href="#">Route Nap</a></li>
+                  </ul>
+                </li>
+                <li><a href="Student.html">Student Info.</a></li>
+                <li><a href="#">Fees Info.</a></li>
+                
+              </ul>
+            </li>
+            <li><a href="contact.html">Contact Us</a></li>
+          </ul>
+        </nav>
+
+      </div>
+    </header>
+
+  </head>
+  <body></body>
+</html>
